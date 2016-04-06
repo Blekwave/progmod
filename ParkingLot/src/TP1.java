e337645197f0e38901bee03d448cefe93ff7c797
Victor Diniz <blek.net@gmail.com>
2016-04-06 16:22:41 -0300

    Split regex compilation from main function in TP1

--- ParkingLot/src/TP1.java
+++ ParkingLot/src/TP1.java
@@ -8,23 +8,27 @@ import java.util.regex.Matcher;
 
 public class TP1
 {
-    public static void main(String[] args)
-    {
-        System.out.println("I guess the program works, let's try reading some stuff.");
-        Scanner input = new Scanner(System.in);
+    private static Pattern compileEventRegex(){
         String eventTypeRegex = "(?<type>[ES])";
         String timeRegex = "(?<time>(?<hours>\\d{2}):(?<minutes>\\d{2}))";
         String licensePlateRegex = "(?<plate>[A-Z]{3}-\\d{4})";
         String vehicleTypeRegex = "(?<vehicle>(?:VP)|(?:VG)|(?:MT)|(?:NE))";
-        Pattern event = Pattern.compile(
+        return Pattern.compile(
             eventTypeRegex + ";" + timeRegex + ";" + licensePlateRegex + ";" + vehicleTypeRegex
         );
+    }
+
+    public static Pattern sEventRegex = compileEventRegex();
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
 
         while (input.hasNext())
         {
             String s = input.next();
             System.out.println("I just read: " + s);
-            Matcher m = event.matcher(s);
+            Matcher m = sEventRegex.matcher(s);
             m.matches();
             String plate = m.group("plate");
             String vehicle = m.group("vehicle");
@@ -39,6 +43,5 @@ public class TP1
                 System.out.println(plate + " (" + vehicle + ") left the parking lot at " + time);
             }
         }
-        System.out.println("Done here, folks.");
     }
 }
