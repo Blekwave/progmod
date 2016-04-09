d314ae5315bb3ca3653d3b0b6b57298bc2dad9b7
Renato Utsch <renatoutsch@gmail.com>
2016-04-09 20:06:50 -0300

    Finish the program. Now fix the bugs.

--- ParkingLot/src/TP1.java
+++ ParkingLot/src/TP1.java
@@ -2,6 +2,7 @@
  * Main class for this assignment. Contains only main(), which deals with stdio and general top-level logic.
  */
 
+import java.io.*;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -20,27 +21,30 @@ public class TP1
 
     public static Pattern sEventRegex = compileEventRegex();
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, ParkingException
     {
         Scanner input = new Scanner(System.in);
+        ParkingLot parkingLot = new ParkingLotBuilder("../parkinglot.config").build();
 
         while (input.hasNext())
         {
             String s = input.next();
-            System.out.println("I just read: " + s);
             Matcher m = sEventRegex.matcher(s);
             m.matches();
             String plate = m.group("plate");
-            String vehicle = m.group("vehicle");
+            String vehicleType = m.group("vehicle");
             String eventType = m.group("type");
-            String time = m.group("time");
-            if (eventType.equals("E"))
-            {
-                System.out.println(plate + " (" + vehicle + ") entered the parking lot at " + time);
+            String hours = m.group("hours");
+            String minutes = m.group("minutes");
+            Integer time = Integer.parseInt(hours) * 60 + Integer.parseInt(minutes);
+
+            Vehicle v = new Vehicle(plate, vehicleType);
+
+            if (eventType.equals("E")) {
+                parkingLot.park(v, time);
             }
-            else
-            {
-                System.out.println(plate + " (" + vehicle + ") left the parking lot at " + time);
+            else {
+                parkingLot.depart(v, time);
             }
         }
     }
