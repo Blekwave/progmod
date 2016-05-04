232c9e013b03ffa00e29c0da300f85952392668f
Victor Diniz <blek.net@gmail.com>
2016-05-04 13:04:14 -0300

    Remove needless dependency

--- Cuttle/src/cuttle/game/cards/behaviors/PointPlayBehavior.java
+++ Cuttle/src/cuttle/game/cards/behaviors/PointPlayBehavior.java
@@ -4,8 +4,6 @@ import cuttle.game.cards.CuttleCard;
 import cuttle.game.cards.prompts.Prompt;
 import cuttle.game.cards.prompts.PromptType;
 
-import java.util.ArrayList;
-
 /**
  * Describe this class and the methods exposed by it.
  */
