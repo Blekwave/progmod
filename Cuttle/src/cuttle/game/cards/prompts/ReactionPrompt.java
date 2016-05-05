80eb2a520dfb7bb6629071f41f2d76f554095f37
Victor Diniz <blek.net@gmail.com>
2016-05-04 22:12:45 -0300

    Set reaction mechanism in ReactionPrompt

--- Cuttle/src/cuttle/game/cards/prompts/ReactionPrompt.java
+++ Cuttle/src/cuttle/game/cards/prompts/ReactionPrompt.java
@@ -4,7 +4,18 @@ package cuttle.game.cards.prompts;
  * Describe this class and the methods exposed by it.
  */
 public class ReactionPrompt extends Prompt {
+    public Boolean reacted(){
+        return mReacted;
+    }
+
+    public void react(){
+        mReacted = true;
+    }
+
+    private Boolean mReacted;
+
     public ReactionPrompt(){
         super(PromptType.ReactionPrompt);
+        mReacted = false;
     }
 }
