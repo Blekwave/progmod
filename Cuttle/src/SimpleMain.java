191888be1b50f85e73a42308dba983e6fd35e6a8
Victor Diniz <blek.net@gmail.com>
2016-05-05 15:39:44 -0300

    Fix SimpleServer main method signature

--- Cuttle/src/SimpleMain.java
+++ Cuttle/src/SimpleMain.java
@@ -4,7 +4,7 @@ import cuttle.simpleserver.SimpleServer;
  * Describe this class and the methods exposed by it.
  */
 public class SimpleMain {
-    public static void main(){
+    public static void main(String[] args){
         new SimpleServer().startGame();
     }
 }
