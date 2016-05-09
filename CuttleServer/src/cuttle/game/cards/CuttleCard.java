55ee7d89ddedc0a08fa4577cc99adf8d49e21d2f
Victor Diniz <blek.net@gmail.com>
2016-05-09 17:20:59 -0300

    Document CuttleCard and Pile

--- CuttleServer/src/cuttle/game/cards/CuttleCard.java
+++ CuttleServer/src/cuttle/game/cards/CuttleCard.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 
 /**
  * Represents a card in a game of Cuttle.
+ *
+ * Has a list of its own behaviors, to be bound to it by subclasses' construc-
+ * tors and a list of events, to be bound to it by behaviors and other in-game
+ * occurrences.
  */
 public abstract class CuttleCard extends PlayingCard {
 
@@ -32,6 +36,14 @@ public abstract class CuttleCard extends PlayingCard {
 
     private final Integer mId;
 
+    /**
+     * Initializes a new CuttleCard.
+     *
+     * @param game Game to which it belongs.
+     * @param id Its numeric ID.
+     * @param suit Its suit.
+     * @param rank Its rank.
+     */
     public CuttleCard(CuttleGame game, Integer id, PlayingCard.Suit suit, PlayingCard.Rank rank){
         super(suit, rank);
         mId = id;
@@ -40,18 +52,39 @@ public abstract class CuttleCard extends PlayingCard {
         mEvents = new ArrayList<>();
     }
 
+    /**
+     * Gets the card's owner.
+     *
+     * @return Player which owns this card or null if it belongs to the deck of
+     *         cards or the scrap pile.
+     */
     public Player owner(){
         return game().cardPile(this).owner();
     }
 
+    /**
+     * Binds a behavior to this card.
+     *
+     * @param cardBehavior Behavior to be bound to the card.
+     */
     public void bindBehavior(CardBehavior cardBehavior){
         mBehaviors.add(cardBehavior);
     }
 
+    /**
+     * Binds an event to this card.
+     *
+     * @param event Event to be bound to the card.
+     */
     public void bindEvent(Event event){
         mEvents.add(event);
     }
 
+    /**
+     * Triggers all events with a given Trigger bound to this card.
+     *
+     * @param trigger Trigger whose events should be triggered.
+     */
     public void trigger(Trigger trigger){
         for (Event event : mEvents){
             if (event.trigger().equals(trigger)){
--- CuttleServer/src/cuttle/game/cards/Pile.java
+++ CuttleServer/src/cuttle/game/cards/Pile.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.Iterator;
 
 /**
- * Describe this class and the methods exposed by it.
+ * Container of CuttleCards. Represented internally by an ArrayList.
  */
 public class Pile implements Iterable<CuttleCard> {
 
@@ -21,14 +21,26 @@ public class Pile implements Iterable<CuttleCard> {
 
     private String mName;
     private Player mOwner;
-    ArrayList<CuttleCard> mList;
+    private ArrayList<CuttleCard> mList;
 
+    /**
+     * Initializes a pile of cards.
+     *
+     * @param name Unique string identifier for this pile.
+     * @param owner Player who owns this pile (null if no player owns it, i.e.
+     *              scrap pile and deck of cards)
+     */
     public Pile(String name, Player owner){
         mName = name;
         mOwner = owner;
         mList = new ArrayList<>();
     }
 
+    /**
+     * Clones (shallowly) an existing pile of cards into a new one.
+     *
+     * @param from Pile which should be copied.
+     */
     public Pile(Pile from){
         this(from.name(), from.owner());
         for (CuttleCard card : from.mList){
@@ -36,40 +48,76 @@ public class Pile implements Iterable<CuttleCard> {
         }
     }
 
+    /**
+     * Removes a card from the pile with a certain index and returns it.
+     *
+     * @param index Index to be removed.
+     * @return CuttleCard removed.
+     */
     public CuttleCard pop(Integer index){
         CuttleCard card = mList.get(index);
         mList.remove(index.intValue());
         return card;
     }
 
+    /**
+     * Removes a card from the top of the pile.
+     *
+     * @return CuttleCard removed.
+     */
     public CuttleCard pop(){
         return pop(mList.size() - 1);
     }
 
+    /**
+     * Places a card at the top of the pile.
+     *
+     * @param card Card to be placed.
+     */
     public void push(CuttleCard card){
         mList.add(card);
     }
 
-    public CuttleCard last(){
-        return mList.get(mList.size() - 1);
-    }
-
+    /**
+     * Returns the number of cards in the pile.
+     *
+     * @return Size of the pile.
+     */
     public Integer size(){
         return mList.size();
     }
 
+    /**
+     * Shuffles the pile.
+     */
     public void shuffle(){
         Collections.shuffle(mList);
     }
 
+    /**
+     * Checks if the pile is empty.
+     *
+     * @return Whether the pile is empty.
+     */
     public Boolean isEmpty(){
         return mList.isEmpty();
     }
 
+    /**
+     * Gets the index of a card in the pile (in linear time).
+     *
+     * @param card Card to be searched for.
+     * @return Index of that card in the pile, -1 if it doesn't belong to it.
+     */
     public Integer indexOf(CuttleCard card){
         return mList.indexOf(card);
     }
 
+    /**
+     * Iterator for the pile of cards.
+     *
+     * @return Iterator.
+     */
     public Iterator<CuttleCard> iterator(){
         return mList.iterator();
     }
