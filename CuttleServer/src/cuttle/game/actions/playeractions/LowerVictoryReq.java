196e2f37b59e38f37bf36253b3be754800e2045c
Victor Diniz <blek.net@gmail.com>
2016-05-10 14:12:42 -0300

    Show new victoryReq in VictoryReq action updates

--- CuttleServer/src/cuttle/game/actions/playeractions/LowerVictoryReq.java
+++ CuttleServer/src/cuttle/game/actions/playeractions/LowerVictoryReq.java
@@ -2,6 +2,7 @@ package cuttle.game.actions.playeractions;
 
 import cuttle.game.Player;
 import cuttle.game.actions.PlayerAction;
+import org.json.JSONObject;
 
 /**
  * Lowers a player's victory requirements (registers there's one less king on
@@ -11,6 +12,20 @@ public class LowerVictoryReq extends PlayerAction {
 
     public LowerVictoryReq(Player player){
         super(player, "lower_victory_req");
+    }
+
+    /**
+     * Builds a JSON update object regarding to the action performed, to be
+     * relayed to the current player. Contains the action's player's ID as
+     * well as the player's new victory requirements.
+     *
+     * @return New JSONObject with the action's execution information.
+     */
+    @Override
+    public JSONObject buildPlayerUpdate(){
+        JSONObject obj = super.buildPlayerUpdate();
+        obj.put("new_victory_req", player().victoryReq());
+        return obj;
     }
 
     @Override
--- CuttleServer/src/cuttle/game/actions/playeractions/RaiseVictoryReq.java
+++ CuttleServer/src/cuttle/game/actions/playeractions/RaiseVictoryReq.java
@@ -2,6 +2,7 @@ package cuttle.game.actions.playeractions;
 
 import cuttle.game.Player;
 import cuttle.game.actions.PlayerAction;
+import org.json.JSONObject;
 
 /**
  * Raises a player's victory requirements (registers there's one extra king on
@@ -11,6 +12,20 @@ public class RaiseVictoryReq extends PlayerAction {
 
     public RaiseVictoryReq(Player player){
         super(player, "raise_victory_req");
+    }
+
+    /**
+     * Builds a JSON update object regarding to the action performed, to be
+     * relayed to the current player. Contains the action's player's ID as
+     * well as the player's new victory requirements.
+     *
+     * @return New JSONObject with the action's execution information.
+     */
+    @Override
+    public JSONObject buildPlayerUpdate(){
+        JSONObject obj = super.buildPlayerUpdate();
+        obj.put("new_victory_req", player().victoryReq());
+        return obj;
     }
 
     @Override
