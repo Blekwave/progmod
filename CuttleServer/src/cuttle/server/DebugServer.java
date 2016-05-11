ddba4a70160f2715059a44902d5190b7a550bcc5
Victor Diniz <blek.net@gmail.com>
2016-05-11 19:11:37 -0300

    Documents the DebugServer

--- CuttleServer/src/cuttle/server/DebugServer.java
+++ CuttleServer/src/cuttle/server/DebugServer.java
@@ -7,25 +7,45 @@ import org.json.JSONObject;
 import java.util.Scanner;
 
 /**
- * Describe this class and the methods exposed by it.
+ * Minimal compact server used for debugging the communication protocol.
  */
 public class DebugServer implements ServerInterface {
     private Scanner mScanner;
 
+    /**
+     * Initializes the DebugServer.
+     */
     public DebugServer(){
         mScanner = new Scanner(System.in);
     }
 
+    /**
+     * Runs the DebugServer, starting a new game of Cuttle between two players.
+     */
     public void run(){
         new CuttleGame(this, 0, 1).start();
     }
 
+    /**
+     * Sends an update to the command line interface.
+     *
+     * @param message JSON message to be communicated.
+     * @param playerId ID of the player to which the update should be sent.
+     */
     @Override
     public void update(JSONObject message, Integer playerId) {
         System.out.println("[UPDATE -> " + playerId + "]");
         System.out.println(message.toString(4));
     }
 
+    /**
+     * Prompts one of the players for choosing an action through the command
+     * line interface.
+     *
+     * @param message Prompt message to be communicated.
+     * @param playerId ID of the player to be prompted.
+     * @return Object corresponding to the chosen BehaviorCall.
+     */
     @Override
     public JSONObject prompt(JSONObject message, Integer playerId) {
         System.out.println("[PROMPT -> " + playerId + "]");
@@ -35,6 +55,12 @@ public class DebugServer implements ServerInterface {
         return buildPromptResponse(id);
     }
 
+    /**
+     * Generates a valid prompt response JSON object from a call ID.
+     *
+     * @param callId ID of the chosen call.
+     * @return JSONObject to be sent as a response to the prompt.
+     */
     private JSONObject buildPromptResponse(Integer callId){
         JSONObject response = new JSONObject();
         response.put("type", "prompt_response");
